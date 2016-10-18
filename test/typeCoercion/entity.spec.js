da27bc312983e938d7930c1c70933da8515a2dfc
Talysson <talyssonoc@gmail.com>
2016-10-18 19:58:35 -0200

    Add tests for POJO class and entities attributes coercion

---
+++ test/typeCoercion/entity.spec.js
@@ -0,0 +1,51 @@
+const { expect } = require('chai');
+const { attributes } = require('../../src');
+
+describe('type coercion', () => {
+  describe('Entity class', () => {
+    const Location = attributes({
+      x: Number,
+      y: Number
+    })(class Location {});
+
+    const User = attributes({
+      location: Location
+    })(class User {});
+
+    it('does not coerce if raw value is an instance of class', () => {
+      const location = new Location({ x: 1, y: 2});
+
+      const user = new User({ location });
+
+      expect(user.location).to.equal(location);
+    });
+
+    it('does not coerce undefined', () => {
+      const user = new User({
+        location: undefined
+      });
+
+      expect(user.location).to.be.undefined;
+    });
+
+    it('instantiates class with raw value', () => {
+      const user = new User({
+        location: { x: 1, y: 2}
+      });
+
+      expect(user.location).to.be.instanceOf(Location);
+      expect(user.location.x).to.equal(1);
+      expect(user.location.y).to.equal(2);
+    });
+
+    it('does nested coercion', () => {
+      const user = new User({
+        location: { x: '1', y: '2'}
+      });
+
+      expect(user.location).to.be.instanceOf(Location);
+      expect(user.location.x).to.equal(1);
+      expect(user.location.y).to.equal(2);
+    });
+  });
+});
