b7dcd1ca6b4b2c1e72dce64fb386be20179ac368
Talysson <talyssonoc@gmail.com>
2016-10-20 16:59:23 -0200

    Move descriptors down so the decorator has better visibility

--- src/attributesDecorator.js
+++ src/attributesDecorator.js
@@ -6,6 +6,63 @@ const createAttrs = () => Object.create(null);
 const SCHEMA = Symbol('schema');
 const ATTRIBUTES = Symbol('attributes');
 
+function attributesDecorator(declaredSchema, ErroneousPassedClass) {
+  if(ErroneousPassedClass) {
+    const errorMessage = `You passed the entity class as the second parameter of entity(). The expected usage is \`entity(schema)(${ ErroneousPassedClass.name || 'EntityClass' })\`.`;
+
+    throw new Error(errorMessage);
+  }
+
+  return function decorator(Class) {
+    const WrapperClass = new Proxy(Class, {
+      construct(target, constructorArgs, newTarget) {
+        const instance = Reflect.construct(target, constructorArgs, newTarget);
+        var passedAttributes = constructorArgs[0];
+
+        if(passedAttributes === undefined) {
+          passedAttributes = {};
+        }
+
+        instance.attributes = passedAttributes;
+
+        return instance;
+      }
+    });
+
+    if(SCHEMA in WrapperClass) {
+      declaredSchema = Object.assign({}, WrapperClass[SCHEMA], declaredSchema);
+    }
+
+    declaredSchema = normalizeSchema(declaredSchema);
+
+    define(WrapperClass, SCHEMA, {
+      value: declaredSchema
+    });
+
+    define(WrapperClass.prototype, SCHEMA, {
+      value: declaredSchema
+    });
+
+    define(WrapperClass.prototype, 'attributes', attributesDescriptor);
+
+    Object.keys(declaredSchema).forEach((attr) => {
+      define(WrapperClass.prototype, attr, {
+        get() {
+          return this.attributes[attr];
+        },
+
+        set(value) {
+          this.attributes[attr] = declaredSchema[attr].coerce(value);
+        }
+      });
+    });
+
+    define(WrapperClass.prototype, 'isValid', validationDescriptor);
+
+    return WrapperClass;
+  };
+}
+
 const attributesDescriptor = {
   get() {
 
@@ -64,62 +121,5 @@ const validationDescriptor = {
     return true;
   }
 };
-
-function attributesDecorator(declaredSchema, ErroneousPassedClass) {
-  if(ErroneousPassedClass) {
-    const errorMessage = `You passed the entity class as the second parameter of entity(). The expected usage is \`entity(schema)(${ ErroneousPassedClass.name || 'EntityClass' })\`.`;
-
-    throw new Error(errorMessage);
-  }
-
-  return function decorator(Class) {
-    const WrapperClass = new Proxy(Class, {
-      construct(target, constructorArgs, newTarget) {
-        const instance = Reflect.construct(target, constructorArgs, newTarget);
-        var passedAttributes = constructorArgs[0];
-
-        if(passedAttributes === undefined) {
-          passedAttributes = {};
-        }
-
-        instance.attributes = passedAttributes;
-
-        return instance;
-      }
-    });
-
-    if(SCHEMA in WrapperClass) {
-      declaredSchema = Object.assign({}, WrapperClass[SCHEMA], declaredSchema);
-    }
-
-    declaredSchema = normalizeSchema(declaredSchema);
-
-    define(WrapperClass, SCHEMA, {
-      value: declaredSchema
-    });
-
-    define(WrapperClass.prototype, SCHEMA, {
-      value: declaredSchema
-    });
-
-    define(WrapperClass.prototype, 'attributes', attributesDescriptor);
-
-    Object.keys(declaredSchema).forEach((attr) => {
-      define(WrapperClass.prototype, attr, {
-        get() {
-          return this.attributes[attr];
-        },
-
-        set(value) {
-          this.attributes[attr] = declaredSchema[attr].coerce(value);
-        }
-      });
-    });
-
-    define(WrapperClass.prototype, 'isValid', validationDescriptor);
-
-    return WrapperClass;
-  };
-}
 
 module.exports = attributesDecorator;
